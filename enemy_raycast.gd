056ec04a29cfe3e228c9361f711be7a032891d6f
semiotix <semiotix@outlook.com>
2024-07-05 16:32:28 +0900

    Signed-off-by: semiotix <semiotix@outlook.com>

---
+++ enemy_raycast.gd
@@ -0,0 +1,27 @@
+extends RayCast3D
+
+# 視界の範囲を設定（例：視界距離10メートル）
+var view_distance: float = 10.0
+var view_angle_min: float = 20.0  # 前方20度
+var view_angle_max: float = 160.0  # 前方110度
+
+func _ready():
+	#RenderingServer.VIEWPORT_DEBUG_DRAW_WIREFRAME
+	# 初期設定などを行う場合はここに記述
+	pass
+
+func _process(delta: float):
+	for angle_deg in range(view_angle_min, view_angle_max, 10):
+		var ray_direction = Vector3(cos(deg_to_rad(angle_deg)),0.0,sin(deg_to_rad(angle_deg))) * view_distance
+		target_position = transform.origin + ray_direction
+		
+		var start_point = global_transform.origin
+		var end_point = start_point + ray_direction
+		# 視界を線で描画
+		DebugDraw3D.draw_line(start_point,end_point,Color(1.0,0.0,0.0,1.0),view_distance)
+		if is_colliding():
+			var collision_point = get_collision_point()
+			var collided_object = get_collider()
+			# 衝突したオブジェクトに対する処理を実装
+			if not (collided_object is StaticBody3D):
+				print("視界内に衝突したオブジェクト:", collided_object)
